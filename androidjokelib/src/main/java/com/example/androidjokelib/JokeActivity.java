43fbae07f00b4f2f5c58e8567f72f1edc5008e6b
Nkt1001 <nkt011094@gmail.com>
2016-10-13 16:36:59 +0300

    commit

--- androidjokelib/src/main/java/com/example/androidjokelib/JokeActivity.java
+++ androidjokelib/src/main/java/com/example/androidjokelib/JokeActivity.java
@@ -2,8 +2,8 @@ package com.example.androidjokelib;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.JokeClass;
